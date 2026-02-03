dd7deebce25b47f99762945249c2a2e1c7f1abf6
Shaan Khan <mrask@gmail.com>
2026-02-03 17:43:41 +0530

    modify main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,18 @@
 terraform {
+  required_version = ">= 1.5.0"
+
   required_providers {
     aws = {
-      source = "hashicorp/aws"
-      version = "6.30.0"
+      source  = "hashicorp/aws"
+      version = "~> 6.30"
     }
+  }
+
+  backend "s3" {
+    bucket         = "ask-terraform-backend"
+    key            = "EKS/backend.tfstate"
+    region         = "eu-north-1"
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
   }
 }
